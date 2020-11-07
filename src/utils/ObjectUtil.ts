0ba244d51d1057a1e1a4f791a803e2acd2bc24fb
Stephen Niedzielski <stephen@niedzielski.com>
2020-11-07 13:35:42 -0700

    [ObjectUtil] Remove unused values() function

--- src/utils/ObjectUtil.ts
+++ src/utils/ObjectUtil.ts
@@ -19,14 +19,6 @@ export namespace ObjectUtil {
   }
 
   // https://github.com/Microsoft/TypeScript/pull/12253
-  export function values<T>(object: Readonly<T & object>): T[keyof T][] {
-    const values = []
-    for (const key in object)
-      if (object.hasOwnProperty(key)) values.push(object[key]!)
-    return values
-  }
-
-  // https://github.com/Microsoft/TypeScript/pull/12253
   export function entries<T>(
     object: Readonly<T & object>
   ): [keyof T, T[keyof T]][] {
