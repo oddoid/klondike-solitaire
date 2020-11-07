29adff2e3d1bd54ce58a306d44b72e7d88485b8c
Stephen Niedzielski <stephen@niedzielski.com>
2020-11-07 13:37:26 -0700

    [Deck] Fix outdated comment on card direction

--- src/layout/Deck.ts
+++ src/layout/Deck.ts
@@ -3,7 +3,7 @@ import {Rank} from '../card/Rank'
 import {Suit} from '../card/Suit'
 
 export namespace Deck {
-  /** Generate a complete pack of 52, face down, and ordered cards. */
+  /** Generate a complete pack of 52 ordered cards. */
   export function make(direction: Card.Direction = 'down'): Card[] {
     const deck = []
     for (const suit of Suit.values)
