ffcbce27e99b994e63d48e867b62d34ce2510752
Stephen Niedzielski <stephen@niedzielski.com>
2020-11-07 22:26:46 -0700

    [ArrayUtil] Remove unused permutations code

--- src/utils/ArrayUtil.test.ts
+++ src/utils/ArrayUtil.test.ts
@@ -35,28 +35,3 @@ function permute<T>(items: T[], n: number = items.length): T[][] {
 
   return permutations
 }
-
-// function factorial(n: number, memo: Record<number, number> = {0: 1}): number {
-//   memo[n] ??= n * factorial(n - 1)
-//   return memo[n]!
-// }
-
-// function permuteWithRepetitions<T>(items: T[]): T[][] {
-//   // In a binary number, each digit has one of two values: 0 or 1. The total
-//   // number of permutations is the number of digits to the number of values
-//   // power. In a byte, that's 2^8. In this case, each item is a "digit" and we
-//   // know that to get every permutation, it needs to be the same length as the
-//   // number of possible values for each digit.
-//   const permutations: T[][] = Array(items.length ** items.length)
-//   for (let i = 0; i < permutations.length; i++) {
-//     const permutation: T[] = Array(items.length)
-//     for (let place = 0, val = i; place < items.length; place++) {
-//       const digit = items[val % items.length]
-//       if (digit) permutation[place] = digit
-//       // Right shift by one by dividing by the radix. For example, in a base-two system the radix is two. items.length is the number of number of possible values in a single digit.
-//       val = ~~(val / items.length)
-//     }
-//     permutations[i] = permutation
-//   }
-//   return permutations
-// }
