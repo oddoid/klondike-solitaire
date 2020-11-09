04e2c20edd05df3c0e1151525972da7d12cf95a2
Stephen Niedzielski <stephen@niedzielski.com>
2020-11-08 19:30:00 -0700

    [ArrayUtil] Replace Map with Record

--- src/utils/ArrayUtil.test.ts
+++ src/utils/ArrayUtil.test.ts
@@ -2,35 +2,35 @@ import {ArrayUtil} from './ArrayUtil'
 
 test('shuffle', () => {
   const iterations = 1_000_000
-  const items = ['a', 'b', 'c', 'd', 'e']
-  const permutations = permute(items).map(permutation => permutation.join(''))
+  const array = ['a', 'b', 'c', 'd', 'e']
+  const permutations = permute(array).map(permutation => permutation.join(''))
 
-  const distribution = new Map<string, number>()
+  const distribution: Record<string, number> = {}
   for (let i = 0; i < iterations; i++) {
-    ArrayUtil.shuffle(items, Math.random)
-    const permutation = items.join('')
-    distribution.set(permutation, (distribution.get(permutation) ?? 0) + 1)
+    ArrayUtil.shuffle(array, Math.random)
+    const permutation = array.join('')
+    distribution[permutation] = (distribution[permutation] ?? 0) + 1
   }
 
   for (const permutation of permutations) {
-    const occurrences = distribution.get(permutation) ?? 0
+    const occurrences = distribution[permutation] ?? 0
     const frequency = occurrences / iterations
-    expect(frequency).toBeGreaterThan(1 / distribution.size - 0.001)
-    expect(frequency).toBeLessThan(1 / distribution.size + 0.001)
+    expect(frequency).toBeGreaterThan(1 / permutations.length - 0.001)
+    expect(frequency).toBeLessThan(1 / permutations.length + 0.001)
   }
 })
 
 /** [Heap's_algorithm](https://en.wikipedia.org/wiki/Heap%27s_algorithm). */
-function permute<T>(items: T[], n: number = items.length): T[][] {
-  if (n === 1) return [[...items]]
+function permute<T>(array: T[], n: number = array.length): T[][] {
+  if (n === 1) return [[...array]]
 
   const permutations = []
-  permutations.push(...permute(items, n - 1))
+  permutations.push(...permute(array, n - 1))
 
   for (let i = 0; i < n - 1; i++) {
-    if (n % 2) ArrayUtil.swap(items, 0, n - 1)
-    else ArrayUtil.swap(items, i, n - 1)
-    permutations.push(...permute(items, n - 1))
+    if (n % 2) ArrayUtil.swap(array, 0, n - 1)
+    else ArrayUtil.swap(array, i, n - 1)
+    permutations.push(...permute(array, n - 1))
   }
 
   return permutations
